dddaa21a068bf5a09dac9435ae28afffcf007145
Aldo Lopez <lopez.aldo.a@gmail.com>
2019-07-29 23:33:57 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-c.jpg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:info@blindedbythelightstudios.com?subject=Blinded by the Light Inquiry"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >  
+          Email Us
+        </a>  
       </header>
     </div>
   );
